ac11b3cd189977a8858e3756a1e16a32a507efc6
fmcpeaimcf <fmcpeaimcf@gmail.com>
2026-01-02 14:23:10 +0000

    uno commit : Configuracion fmcproyectopeaimcf  Vite+React+Typescript+tailwinds+Firebase.

--- src/App.jsx
+++ src/App.jsx
@@ -26,6 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
+        Nueva Version 2.0.0
         Click on the Vite and React logos to learn more
       </p>
     </>
